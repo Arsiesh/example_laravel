d02f774209392d7a737599c4a60344b7c58804a1
Arsiesh <raphaelsacdalan@yahoo.com.ph>
2024-04-24 17:32:44 +0800

    Add Controller Functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
  
 use Illuminate\Http\Request;
- 
+use App\Models\Customer;
 class UserController extends Controller
 {
     /**
@@ -12,13 +12,60 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
+    //  store
+    //  update
+    //  delete
+    //  show/{id}
+    //  show-all
     public function store(Request $request)
     {
-        $username = $request?->username;
-        $first = $request?->first;
-        $last = $request?->last;
-        $phone = $request?->phone;     
-    
 
+
+        $user = Customer::create($request->all());
+        $user->store([
+            "first"=> $request->first,
+            "last"=> $request->last,
+            "phone"=> $request->phone,
+        ]);
+        // $username = $request?->username;
+        // $first = $request?->first;
+        // $last = $request?->input('last');
+        // $phone = $request?->input('phone');     
+
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $user = Customer::findOrFail($id);
+
+        $user->update([
+            "first"=> $request->first,
+            "last"=> $request->last,
+            "phone"=> $request->phone,
+        ]);
+
+        // $user->username = $request->input('username', $user->username);
+        // $user->first = $request->input('first', $user->first);
+        // $user->last = $request->input('last', $user->last);
+        // $user->phone = $request->input('phone', $user->phone);
+
+        $user->save();
+    }
+
+    public function destroy($id){
+        $user = Customer::findOrFail($id);
+        $user->delete();
+    }
+
+    public function show($id){
+        $user = Customer::findOrFail($id);
+        return response()->json($user);
+    }
+
+    public function showAll(){
+        $user = Customer::all();
+        return response()->json($user);
     }
 }
